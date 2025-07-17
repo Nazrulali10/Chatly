c8ca538c9c9feed5ba147324a6fe3541cd3c5548
Nazrul Ali.N <nazrulali555@gmail.com>
2025-07-17 12:12:29 +0530

    VITE URL

--- client/src/config.jsx
+++ client/src/config.jsx
@@ -1,6 +1,6 @@
 const BASE_URL =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:5001/api'
-    : '/api';
+    : `${import.meta.env.VITE_BACKEND_URL}/api`;
 
 export default BASE_URL;
