c0b723a3cfd1420a912c3c9bdaee216adebfd36e
Nazrul Ali.N <nazrulali555@gmail.com>
2025-07-17 11:50:25 +0530

    index fix2

--- server/mongoose.js
+++ server/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 async function connectDB() {
     try {
-        const connect = await mongoose.connect(process.env.MONGODBURI)
+        const connect = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`mongodb connected successfully ${connect.connection.host}`)
     } 
     catch (error) {
