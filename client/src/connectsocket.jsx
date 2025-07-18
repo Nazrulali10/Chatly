245b289ae5268a0bc25d529367622c0e0579a0ad
Nazrul Ali.N <nazrulali555@gmail.com>
2025-07-18 09:24:38 +0530

    socket

--- client/src/connectsocket.jsx
+++ client/src/connectsocket.jsx
@@ -1,11 +1,12 @@
 import { io } from 'socket.io-client';
+import BASE_URL from './config';
 
 const connectSocket = (userid,setsocket,socket,setonlineUsers,) =>{
         if(userid){
             if (socket) {
                 socket.disconnect(); 
             }
-        const newsocket =io(import.meta.env.MODE==="development"?"http://localhost:5001":"/",{query:{userid:userid,withCredentials:true}})
+        const newsocket =io(import.meta.env.MODE==="development"?"http://localhost:5001":"https://chatly-t9rk.onrender.com",{query:{userid:userid,withCredentials:true}})
         newsocket.connect()
         setsocket(newsocket)
         newsocket.on("getonlineusers",(userids)=>{
