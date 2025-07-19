b9f0d9fbb4f0da358469a1e2be6bc94a8a777f19
Nazrul Ali.N <nazrulali555@gmail.com>
2025-07-19 11:18:46 +0530

    c

--- client/src/pages/Profilepage.jsx
+++ client/src/pages/Profilepage.jsx
@@ -33,9 +33,9 @@ export default function Profilepage({
   };
   return (
     
-      <div className="bg-sky-500 flex h-screen w-full items-center px-10 md:p-0 md:justify-center md:items-center">
-        <div className="flex flex-col bg-indigo-950  h-100 md:h-120 w-100 md:w-120 p-3 md:p-5 justify-center items-center">
-          <h1 className="text-3xl md:text-4xl text-white font-bold">Profile</h1>
+      <div className="bg-sky-500 flex h-screen w-full items-center justify-center px-10 md:p-0 md:justify-center md:items-center">
+        <div className="flex flex-col bg-indigo-950  h-90 md:h-120 w-80 md:w-120 p-5 md:p-5 justify-center items-center">
+          <h1 className="text-2xl md:text-4xl text-white font-bold">Profile</h1>
           <div className="mt-4 relative ">
             <img
               className="border-2 border-white rounded-full h-35 w-35 md:h-50 md:w-50"
@@ -56,13 +56,13 @@ export default function Profilepage({
               accept="image/*"
             />
           </div>
-          <span className="text-lime-400 mb-3 font-semibold">Active</span>
+          <span className="text-lime-400 mb-3 text-sm font-semibold">Active</span>
 
           <label className="text-white font-semibold text-sm">
              Name:
             <input
               type="text"
-              className="w-60 md:w-80 border ml-2 bordee-1 font-semibold border-md px-3 py-2 text-white border-sky-500 mb-4 text-xs"
+              className="w-50 md:w-80 border ml-2 font-semibold border-md px-3 py-2 text-white border-sky-500 mb-4 text-xs"
               value={authUser.fullname}
               readOnly
             />
@@ -71,7 +71,7 @@ export default function Profilepage({
             Email:{" "}
             <input
               type="text"
-              className="w-60 md:w-80 border ml-2 bordee-1 font-semibold border-md px-3 py-2 text-white border-sky-500 text-xs"
+              className="w-50 md:w-80 border ml-2  font-semibold border-md px-3 py-2 text-white border-sky-500 text-xs"
               value={authUser.email}
               readOnly
             />
